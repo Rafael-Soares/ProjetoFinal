55cdc79664bbc37808db942a5c26381d47b3d1f9
Rafael-Soares <rafaelsoaresif@gmail.com>
2021-04-14 17:21:26 -0300

    integração das seções: home, quem somos e pacotes ao banco de dados e alteração nos modelos

--- DesenvolvimentoBack/apps/institucional/models.py
+++ DesenvolvimentoBack/apps/institucional/models.py
@@ -1,21 +1,20 @@
 from django.db import models
 from datetime import datetime
    
-class home(models.Model):
-    #slogan_home = models.CharField('Slogan do Home', max_length=255, blank=True) 
+class home(models.Model): 
     quantidade_parcelas = models.IntegerField('Quantidade de Parcelas', default=0)
     preco_card = models.FloatField('Preço do Card', default=0)
     descricao_card = models.TextField('Descrição do Card', blank=True)
     data_modificacao = models.DateTimeField('Data de modificação', default=datetime.now, blank=True)
 
 class quemSomos(models.Model):
-    titulo = models.CharField('Título', max_length=50, blank=True)
     oferecimento = models.TextField('O que oferecemos', blank=True)
     oque_fazemos = models.TextField('O que fazemos', blank=True)
     valores = models.TextField('Valores da instituição', blank=True)
     data_modificacao = models.DateTimeField('Data de modificação', default=datetime.now, blank=True)
 
 class servicos(models.Model):
+    logo_servico = models.ImageField('Logo do serviço', upload_to='imagens/', blank=True)
     nome = models.CharField('Nome do serviço', max_length=50, blank=True)
     descrição_do_servico = models.TextField('Descrição', blank=True)
     data_modificacao = models.DateTimeField('Data de modificação', default=datetime.now, blank=True)
@@ -33,5 +32,4 @@ class pacotes(models.Model):
     valor_parcela = models.FloatField('Valor da parcela', default= 0)
     valor_a_vista = models.FloatField('Valor à vista', default=0)
     desconto = models.IntegerField('Porcentagem de desconto', default=0)
-    atributos_pacote = models.TextField('Atributos do pacote', blank=True)
     data_modificacao = models.DateTimeField('Data de modificação', default=datetime.now, blank=True)
