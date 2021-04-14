b8d1aaab915fe09c433e0b44efd6d6d50b697459
GilsonKedson <gilsonkedson15@outlook.com>
2021-04-14 10:03:31 -0300

    Banco de dados SQLite3

--- DesenvolvimentoBack/pasta_raiz/settings.py
+++ DesenvolvimentoBack/pasta_raiz/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path, os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -64,11 +63,9 @@ WSGI_APPLICATION = 'pasta_raiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aprender_e_crescer',
-        'USER': 'postgres',
-        'PASSWORD': 'treineeEject', # SENHAS ->  KEDSON -> 15022001ke    RAFAEL ->  treineeEject
-        'HOST': 'localhost'
+        #BANCO SQLITE3
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '../db.sqlite3',
     }
 }
 
